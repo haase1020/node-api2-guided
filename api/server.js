b402c4698f0b04dfb04c9b1ca020b5c276fe8d32
Mandi Haase <haase1020@gmail.com>
2020-03-10 14:25:50 -0400

    updated code from lecture

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const hubsRouter = require('../hubs/hubs-router.js');
+const server = express();
+server.use(express.json());
+server.get('/', (req, res) => {
+    res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+server.use('/api/hubs', hubsRouter);
+module.exports = server;
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
